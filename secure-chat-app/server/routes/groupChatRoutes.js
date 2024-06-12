d85bac92f30a9f63b129d9976d500deaa7d35a5e
Jesus165 <elijahmorgan16514@gmail.com>
2024-06-11 23:10:22 -0400

    idk

--- secure-chat-app/server/routes/groupChatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createGroupChat, sendMessage, getGroupChats } = require('../controllers/groupChatController');
-
-router.post('/create', createGroupChat);
-router.post('/send', sendMessage);
-router.get('/', getGroupChats);
-
-module.exports = router;
